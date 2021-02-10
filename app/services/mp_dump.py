8d10867bf87c31d8f850faad0090ea98d80b6bc5
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2021-02-10 15:14:46 +0100

    [#8] MongoDB integration

---
+++ app/services/mp_dump.py
@@ -0,0 +1,19 @@
+# pylint: disable=missing-function-docstring, no-member
+
+"""Functions to handle marketplace datbase dump"""
+
+from inflection import underscore, pluralize
+from app.db.mongo_models import MP_DUMP_MODEL_CLASSES
+
+
+def load_mp_dump(data):
+    for model in MP_DUMP_MODEL_CLASSES:
+        json_key = pluralize(underscore(model.__name__))
+        for json_dict in data[json_key]:
+            model(**json_dict).save()
+
+
+def drop_mp_dump():
+    for model in MP_DUMP_MODEL_CLASSES:
+        for obj in model.objects:
+            obj.delete()
