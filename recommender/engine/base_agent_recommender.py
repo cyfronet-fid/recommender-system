44fe2c7409168d9dafafcb2612b11b66a86129e0
Jan Kapała <jankapala@gmail.com>
2021-05-13 10:18:02 +0200

    [#60] Base Agent Recommender

--- recommender/engine/base_agent_recommender.py
+++ recommender/engine/base_agent_recommender.py
@@ -1,10 +1,18 @@
-import random
-from typing import Dict, Any, Tuple
+# pylint: disable=invalid-name, no-self-use, no-member, too-few-public-methods
 
-from engine.panel_id_to_services_number_mapping import PANEL_ID_TO_K
-from engine.pre_agent.pre_agent import InvalidRecommendationPanelIDError, _fill_candidate_services, _services_to_ids
-from models import User, SearchData
-from services.fts import retrieve_services
+"""Implementation of the base agent recommmender. Every other agent should
+ inherit from this one."""
+
+import random
+from typing import Dict, Any, List
+
+from recommender.errors import (
+    InvalidRecommendationPanelIDError,
+    TooSmallRecommendationSpace,
+)
+from recommender.engine.panel_id_to_services_number_mapping import PANEL_ID_TO_K
+from recommender.models import User, SearchData, Service
+from recommender.services.fts import retrieve_services
 
 
 class BaseAgentRecommender:
@@ -17,9 +25,7 @@ class BaseAgentRecommender:
         Initialization function.
         """
 
-        pass
-
-    def call(self, context: Dict[str, Any]) -> Tuple[int]:
+    def call(self, context: Dict[str, Any]) -> List[int]:
         """
         This function allows for getting recommended services for the
         recommendation endpoint based on recommendation context.
@@ -38,15 +44,13 @@ class BaseAgentRecommender:
 
         if user is not None:
             return self._for_logged_user(user, search_data, K)
-        else:
-            return self._for_anonymous_user(search_data, K)
+        return self._for_anonymous_user(search_data, K)
 
     def _load_models(self) -> None:
         """
         It loads model or models needed for recommendations and raise
          exception if it is not available in the database.
-         """
-        pass
+        """
 
     def _get_K(self, context: Dict[str, Any]) -> int:
         """
@@ -81,31 +85,61 @@ class BaseAgentRecommender:
 
         return user
 
-    def _for_logged_user(self, user: User, search_data: SearchData, k: int) -> Tuple[int]:
+    def _for_logged_user(
+        self, user: User, search_data: SearchData, k: int
+    ) -> List[int]:
         """
         Generate recommendation for logged user
 
         Args:
             user: user for whom recommendation will be generated.
-            search_data: search phrase and filters information for narrowing down an item space.
+            search_data: search phrase and filters information for narrowing
+             down an item space.
 
         Returns:
-            Tuple of recommended services ids.
+            List of recommended services ids.
         """
-        pass
 
-    def _for_anonymous_user(self, search_data: SearchData, K: int) -> Tuple[int]:
+    def _for_anonymous_user(self, search_data: SearchData, K: int) -> List[int]:
         """
         Generate recommendation for anonymous user
 
         Args:
-            search_data: search phrase and filters information for narrowing down an item space.
+            search_data: search phrase and filters information for narrowing
+             down an item space.
 
         Returns:
             Tuple of recommended services ids.
         """
 
         candidate_services = list(retrieve_services(search_data))
-        candidate_services = _fill_candidate_services(candidate_services, K)
+        candidate_services = self._fill_candidate_services(candidate_services, K)
         recommended_services = random.sample(list(candidate_services), K)
-        return _services_to_ids(recommended_services)
+        return [s.id for s in recommended_services]
+
+    def _fill_candidate_services(
+        self, candidate_services, required_services_no, accessed_services_ids=None
+    ):
+        """Fallback in case of len of the candidate_services being too small"""
+
+        if len(candidate_services) < required_services_no:
+            missing_n = required_services_no - len(candidate_services)
+
+            # We don't want to recommend same service multiple times
+            # in one recommendation panel
+            forbidden_ids = {s.id for s in candidate_services}
+
+            # We don't want to recommend a service if it already
+            # has been ordered by a user
+            if accessed_services_ids is not None:
+                forbidden_ids = forbidden_ids | set(accessed_services_ids)
+
+            allowed_services = list(Service.objects(id__nin=forbidden_ids))
+
+            if len(allowed_services) >= missing_n:
+                sampled = random.sample(allowed_services, missing_n)
+                candidate_services += sampled
+            else:
+                raise TooSmallRecommendationSpace
+
+        return candidate_services
--- recommender/engine/pre_agent/pre_agent.py
+++ recommender/engine/pre_agent/pre_agent.py
@@ -3,21 +3,19 @@
 
 """Implementation of the recommender engine Pre-Agent"""
 
-import random
+from typing import List
+
 import torch
 
-from recommender.errors import (
-    InvalidRecommendationPanelIDError,
-    UntrainedPreAgentError,
-    TooSmallRecommendationSpace,
-)
+from recommender.models import SearchData
+from recommender.engine.base_agent_recommender import BaseAgentRecommender
+from recommender.errors import UntrainedPreAgentError
 from recommender.engine.pre_agent.models.neural_colaborative_filtering import NEURAL_CF
 from recommender.engine.pre_agent.preprocessing.preprocessing import (
     user_and_services_to_tensors,
 )
-from recommender.models import User, Service
+from recommender.models import User
 
-from recommender.engine.panel_id_to_services_number_mapping import PANEL_ID_TO_K
 from recommender.engine.pre_agent.models.common import (
     load_last_module,
     NoSavedModuleError,
@@ -25,50 +23,17 @@ from recommender.engine.pre_agent.models.common import (
 from recommender.services.fts import retrieve_services
 
 
-def get_accessed_services_ids(user):
-    return [s.id for s in user.accessed_services]
-
-
-def _services_to_ids(services):
-    return [s.id for s in services]
-
-
-def _fill_candidate_services(
-    candidate_services, required_services_no, accessed_services_ids=None
-):
-    """Fallback in case of len of the candidate_services being too small"""
-    if len(candidate_services) < required_services_no:
-        missing_n = required_services_no - len(candidate_services)
-
-        # We don't want to recommend same service multiple times
-        # in one recommendation panel
-        forbidden_ids = set(_services_to_ids(candidate_services))
-
-        # We don't want to recommend a service if it already
-        # has been ordered by a user
-        if accessed_services_ids is not None:
-            forbidden_ids = forbidden_ids | set(accessed_services_ids)
-
-        allowed_services = list(Service.objects(id__nin=forbidden_ids))
-
-        if len(allowed_services) >= missing_n:
-            sampled = random.sample(allowed_services, missing_n)
-            candidate_services += sampled
-        else:
-            raise TooSmallRecommendationSpace
-
-    return candidate_services
-
-
-class PreAgentRecommender:
+class PreAgentRecommender(BaseAgentRecommender):
     """Pre-Agent Recommender based on Neural Collaborative Filtering"""
 
     def __init__(self, neural_cf_model=None):
+        super().__init__()
         self.neural_cf_model = neural_cf_model
 
-    def call(self, context):
-        """This function allows for getting recommended services for the
-        recommendation endpoint based on recommendation context
+    def _load_models(self) -> None:
+        """
+        It loads model or models needed for recommendations and raise
+         exception if it is not available in the database.
         """
 
         if self.neural_cf_model is None:
@@ -77,28 +42,27 @@ class PreAgentRecommender:
             except NoSavedModuleError as no_saved_module:
                 raise UntrainedPreAgentError from no_saved_module
 
-        k = PANEL_ID_TO_K.get(context["panel_id"])
-        if k is None:
-            raise InvalidRecommendationPanelIDError
+    def _for_logged_user(
+        self, user: User, search_data: SearchData, k: int
+    ) -> List[int]:
+        """
+        Generate recommendation for logged user.
 
-        search_data = context.get("search_data")
+        Args:
+            user: user for whom recommendation will be generated.
+            search_data: search phrase and filters information for narrowing
+             down an item space.
 
-        user = None
-        if context.get("user_id") is not None:
-            user = User.objects(id=context.get("user_id")).first()
+        Returns:
+            List of recommended services ids.
+        """
 
-        if user:
-            return self._for_logged_user(user, search_data, k)
-        else:
-            return self._for_anonymous_user(search_data, k)
-
-    def _for_logged_user(self, user, search_data, k):
-        accessed_services_ids = get_accessed_services_ids(user)
+        accessed_services_ids = [s.id for s in user.accessed_services]
         candidate_services = list(retrieve_services(search_data, accessed_services_ids))
-        recommended_services = _fill_candidate_services(
+        recommended_services = self._fill_candidate_services(
             candidate_services, k, accessed_services_ids
         )
-        recommended_services_ids = _services_to_ids(recommended_services)
+        recommended_services_ids = [s.id for s in recommended_services]
 
         (
             users_ids,
@@ -116,9 +80,3 @@ class PreAgentRecommender:
         )[:k]
 
         return [pair[1] for pair in top_k]
-
-    def _for_anonymous_user(self, search_data, k):
-        candidate_services = list(retrieve_services(search_data))
-        candidate_services = _fill_candidate_services(candidate_services, k)
-        recommended_services = random.sample(list(candidate_services), k)
-        return _services_to_ids(recommended_services)
