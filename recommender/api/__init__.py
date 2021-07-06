5b835a3615e7157c0ffe594d18692d47480c03ce
Jan Kapała <jankapala@gmail.com>
2021-07-06 09:39:44 +0200

    [#114] Reload task doesn't properly reload agents

--- recommender/api/__init__.py
+++ recommender/api/__init__.py
@@ -7,9 +7,7 @@ from recommender.api.endpoints.recommendations import api as recommendations_ns
 from recommender.api.endpoints.database_dumps import api as database_dumps_ns
 from recommender.api.endpoints.user_actions import api as user_actions_ns
 from recommender.api.endpoints.training import api as training_ns
-from recommender.api.endpoints.reload import api as reload_ns
 from recommender.api.endpoints.update import api as update_ns
-
 
 
 api = Api(
@@ -27,5 +25,4 @@ api.add_namespace(recommendations_ns)
 api.add_namespace(database_dumps_ns)
 api.add_namespace(user_actions_ns)
 api.add_namespace(training_ns)
-api.add_namespace(reload_ns)
 api.add_namespace(update_ns)
--- recommender/tasks/neural_networks.py
+++ recommender/tasks/neural_networks.py
@@ -4,10 +4,11 @@
 """This is example of training and inferencing
  using Neural Colaborative Filtering model.
  """
-from flask import current_app
 
-from recommender.engine.agents.pre_agent.pre_agent import PRE_AGENT, PreAgent
-from recommender.engine.agents.rl_agent.rl_agent import RL_AGENT, RLAgent
+from dotenv import dotenv_values, find_dotenv
+
+from recommender.engine.agents.pre_agent.pre_agent import PRE_AGENT
+from recommender.engine.agents.rl_agent.rl_agent import RL_AGENT
 from recommender.engine.agents.rl_agent.training.common import rl_agent_training
 from recommender.engine.agents.pre_agent.datasets.all_datasets import create_datasets
 from recommender.engine.agents.pre_agent.training.common import pre_agent_training
@@ -16,34 +17,11 @@ from recommender.extensions import celery
 
 
 @celery.task
-def execute_pre_agent_training():
-    pre_agent_training()
-
-
-@celery.task
-def execute_rl_agent_training():
-    rl_agent_training()
-
-
-@celery.task
 def execute_training(_=None):
-    if current_app.config["AGENT_VERSION"] == PRE_AGENT:
-        pre_agent_training()
-    elif current_app.config["AGENT_VERSION"] == RL_AGENT:
-        rl_agent_training()
-    else:
-        current_app.recommender_engine = PreAgent()
-
-
-@celery.task
-def reload(_=None):
-    with current_app.app_context():
-        if current_app.config["AGENT_VERSION"] == PRE_AGENT:
-            current_app.recommender_engine = PreAgent()
-        elif current_app.config["AGENT_VERSION"] == RL_AGENT:
-            current_app.recommender_engine = RLAgent()
-        else:
-            current_app.recommender_engine = PreAgent()
+    agent_version = dotenv_values(find_dotenv())["AGENT_VERSION"]
+    trainings = {PRE_AGENT: pre_agent_training, RL_AGENT: rl_agent_training}
+    training = trainings.get(agent_version, pre_agent_training)
+    training()
 
 
 @celery.task
