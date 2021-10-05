6b06a2fbf16be717d9391ce8f334411fcf825331
wujuu <patryk.zbigniew.wojtowicz@gmail.com>
2021-10-05 14:19:47 +0200

    [#166] Refactor service engagement

--- tests/services/synthetic_dataset/test_service_engagement.py
+++ tests/services/synthetic_dataset/test_service_engagement.py
@@ -7,6 +7,9 @@ import pytest
 
 from recommender.services.synthetic_dataset.service_engagement import (
     approx_service_engagement,
+    _distance_metric,
+    _compute_distance_score,
+    _compute_overlap_score,
 )
 from tests.factories.marketplace import UserFactory, ServiceFactory
 from tests.factories.marketplace.category import CategoryFactory
@@ -66,6 +69,28 @@ def index_id_map():
     return pd.DataFrame([2, 4, 6, 8], columns=["id"])
 
 
+def test__distance_measure(normalized_embedded_services):
+    cluster = normalized_embedded_services[1:]
+    point = normalized_embedded_services[0]
+    calculated_distance = _distance_metric(cluster, point)
+    desired_distance = 0.5779
+    assert np.isclose(calculated_distance, desired_distance, atol=1e-4)
+
+
+def test__compute_distance_score(services, normalized_embedded_services, index_id_map):
+    calculated_score = _compute_distance_score(
+        services[:-1], services[-1], normalized_embedded_services, index_id_map
+    )
+    desired_score = 0.4696
+    assert np.isclose(calculated_score, desired_score, atol=1e-4)
+
+
+def test__compute_overlap_score(user, services):
+    calculated_score = _compute_overlap_score(user, services[0])
+    desired_score = 0.9525
+    assert np.isclose(calculated_score, desired_score, atol=1e-4)
+
+
 def test_approx_service_engagement(
     user, services, normalized_embedded_services, index_id_map
 ):
@@ -74,13 +99,17 @@ def test_approx_service_engagement(
             user, services[2], services[:2], normalized_embedded_services, index_id_map
         )
     )
-    desired_s6 = 0.5605
-    assert np.isclose(calculated_s6, desired_s6, atol=1e-04)
+    desired_s6 = 0.8171
+    assert np.isclose(calculated_s6, desired_s6, atol=1e-4)
 
     calculated_s8 = np.array(
         approx_service_engagement(
-            user, services[3], services[:2], normalized_embedded_services, index_id_map
+            user,
+            services[-1],
+            services[:-1],
+            normalized_embedded_services,
+            index_id_map,
         )
     )
-    desired_s8 = 0.4764
-    assert np.isclose(calculated_s8, desired_s8, atol=1e-04)
+    desired_s8 = 0.7111
+    assert np.isclose(calculated_s8, desired_s8, atol=1e-4)
