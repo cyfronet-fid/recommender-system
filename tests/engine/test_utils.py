ac11871d9b794585ff51872f902073ba1016f095
wujuu <patryk.zbigniew.wojtowicz@gmail.com>
2021-10-06 15:26:07 +0200

    Add itemspace test

--- tests/engine/test_utils.py
+++ tests/engine/test_utils.py
@@ -1,22 +1,33 @@
 # pylint: disable-all
-
+import numpy as np
 import pandas as pd
 import pytest
 import torch
 
-from recommender.engine.agents.rl_agent.utils import get_service_indices
+from recommender.engine.agents.rl_agent.utils import (
+    get_service_indices,
+    create_itemspace,
+)
+from recommender.engine.models.autoencoders import ServiceAutoEncoder, create_embedder
+from tests.factories.marketplace import ServiceFactory
 
 
 @pytest.fixture
-def service_embeddings():
-    return torch.Tensor(
-        [
-            [0.9, 0.5, 1.6, -2.0],  # id=2
-            [0.8, -1.5, 3.0, 1.0],  # id=4
-            [-0.01, 0.7, 1.9, 1.2],  # id=6
-            [0.65, 0.3, -1.6, 0.45],  # id=8
-        ]
-    )
+def proper_parameters():
+    SOH = 10
+    SE = 4
+    return SOH, SE
+
+
+@pytest.fixture
+def services(mongo, proper_parameters):
+    SOH, _ = proper_parameters
+    return [
+        ServiceFactory(id=2, tensor=torch.randint(2, (SOH,))),
+        ServiceFactory(id=8, tensor=torch.randint(2, (SOH,))),
+        ServiceFactory(id=4, tensor=torch.randint(2, (SOH,))),
+        ServiceFactory(id=6, tensor=torch.randint(2, (SOH,))),
+    ]
 
 
 @pytest.fixture
@@ -24,8 +35,18 @@ def index_id_map():
     return pd.DataFrame([2, 4, 6, 8], columns=["id"])
 
 
-def test_get_service_indices(service_embeddings, index_id_map):
+def test_get_service_indices(index_id_map):
     assert get_service_indices(index_id_map, [4, 6]) == [1, 2]
     assert get_service_indices(index_id_map, [10]) == []
     assert get_service_indices(index_id_map, []) == []
     assert get_service_indices(index_id_map, [8, 2]) == [3, 0]
+
+
+def test_create_itemspace(services, proper_parameters):
+    SOH, SE = proper_parameters
+    embedder = create_embedder(ServiceAutoEncoder(features_dim=SOH, embedding_dim=SE))
+    itemspace, index_id_map = create_itemspace(embedder)
+    assert itemspace.shape == torch.Size([len(services), SE])
+    assert index_id_map.size == len(services)
+    assert (index_id_map.id.values == np.array([2, 4, 6, 8])).all()
+    assert (index_id_map.index.values == np.array([0, 1, 2, 3])).all()
