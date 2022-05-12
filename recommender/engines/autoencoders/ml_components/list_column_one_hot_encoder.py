e3cc51c378688a6b8b91ab5aeee49bc6ce32ae0c
mich.kolomanski <mich.kolomanski@gmail.com>
2022-05-12 15:36:09 +0200

    [#365] Fix pandas warnings in AE pipeline

--- recommender/engines/autoencoders/ml_components/list_column_one_hot_encoder.py
+++ recommender/engines/autoencoders/ml_components/list_column_one_hot_encoder.py
@@ -1,12 +1,18 @@
-# pylint: disable=keyword-arg-before-vararg, invalid-name, unused-argument, missing-function-docstring
+# pylint: disable=keyword-arg-before-vararg, invalid-name, unused-argument, missing-function-docstring, line-too-long
 
 """This module contains custom encoder needed for preprocessing"""
 
+from __future__ import annotations
+from warnings import simplefilter
 from sklearn.base import BaseEstimator, TransformerMixin
+import pandas as pd
+
+# There is a performance warning because pandas insert is used, but it doesn't matter at that scale.
+simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 
 
 class ListColumnOneHotEncoder(BaseEstimator, TransformerMixin):
-    """This encoder get column(s) of rows filled with list of values
+    """This encoder gets column(s) of rows filled with list of values
     and transform them using on-hot encoding with new column for each
     unique value in all lists of the column"""
 
@@ -14,33 +20,32 @@ class ListColumnOneHotEncoder(BaseEstimator, TransformerMixin):
         self.unique_values = {}
         self.old_columns = None
 
-    def fit(self, X, y=None, *args, **kwargs):
-        for column_name in X.columns:
+    def fit(self, df: pd.DataFrame) -> ListColumnOneHotEncoder:
+        for column_name in df.columns:
             self.unique_values[column_name] = set()
 
-        for column_name in X.columns:
-            for row in X[column_name]:
+        for column_name in df.columns:
+            for row in df[column_name]:
                 for entry in row:
                     self.unique_values.get(column_name).add(entry)
 
             sorted_list = sorted(list(self.unique_values.get(column_name)))
             self.unique_values[column_name] = sorted_list
-
         return self
 
-    def transform(self, X, *args, **kwargs):
-        self.old_columns = X.columns
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        self.old_columns = df.columns
         for old_column in self.old_columns:
             for unique_value in self.unique_values[old_column]:
                 new_column = (old_column, unique_value)
-                X.insert(len(X.columns), new_column, 0)
+                df.insert(len(df.columns), new_column, 0)
 
-        X = X.apply(self.set_values, axis="columns")
+        df = df.apply(self.set_values, axis="columns")
 
-        X.drop(columns=self.old_columns, inplace=True)
-        return X
+        df.drop(columns=self.old_columns, inplace=True)
+        return df
 
-    def set_values(self, row):
+    def set_values(self, row: pd.Series) -> pd.Series:
         """utility method used for transforming each row"""
         for old_column in self.old_columns:
             for unique_value in row[old_column]:
